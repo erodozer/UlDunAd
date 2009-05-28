29376834b935226eedaf76bc66817d4b559290f0
Nicholas Hydock <n_hydock@comcast.net>
2009-05-28 20:02:58 +0000

    Fixes player stats and .ini loading
    git-svn-id: http://uldunad.googlecode.com/svn/trunk@11 16632290-4711-11de-b083-b7e289dd2314

--- Player.py
+++ Player.py
@@ -24,6 +24,7 @@
 import GameEngine
 
 import Config
+import os
 
 player = GameEngine.player
 playerini = Config.Configuration(os.path.join("Data", "Players", player)).player
@@ -32,13 +33,13 @@ race = playerini.race
 raceini = Config.Configuration(os.path.join("Data", "Races", race)).race
 levelcurve = raceini.levelcurve.split(",")
 lvl = playerini.lvl
-hp = raceini.hp + (levelcurve[0]*lvl)
-sp = raceini.sp + (levelcurve[1]*lvl)
-atk = raceini.atk + (levelcurve[2]*lvl)
-defn = raceini.defn + (levelcurve[3]*lvl)
-spd = raceini.spd + (levelcurve[4]*lvl)
-mag = raceini.mag + (levelcurve[5]*lvl)
-evd = raceini.evd + (levelcurve[6]*lvl)
+hp = raceini.hp + (levelcurve[0]*int(lvl))
+sp = raceini.sp + (levelcurve[1]*int(lvl))
+atk = raceini.atk + (levelcurve[2]*int(lvl))
+defn = raceini.defn + (levelcurve[3]*int(lvl))
+spd = raceini.spd + (levelcurve[4]*int(lvl))
+mag = raceini.mag + (levelcurve[5]*int(lvl))
+evd = raceini.evd + (levelcurve[6]*int(lvl))
 weapon = playerini.weapon
 armor = playerini.armor
 
