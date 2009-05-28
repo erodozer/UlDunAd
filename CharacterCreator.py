b67d5913f31e0ce4f8cb1106b92c62e19e917add
Nicholas Hydock <n_hydock@comcast.net>
2009-05-28 20:01:59 +0000

    Basic Character creation started Letter selection code is now even smaller Some other code cleanup
    git-svn-id: http://uldunad.googlecode.com/svn/trunk@10 16632290-4711-11de-b083-b7e289dd2314

--- CharacterCreator.py
+++ CharacterCreator.py
@@ -36,19 +36,19 @@ class CharacterCreator(Layer):
     self.race = "Hume.ini"
     raceini = Config.Configuration(os.path.join("Data", "Races", self.race)).race
 
-    self.hp = raceini.hp
-    self.sp = raceini.sp
-    self.atk = raceini.atk
-    self.defn = raceini.defn
-    self.spd = raceini.spd
-    self.mag = raceini.mag
-    self.evd = raceini.evd
+    self.stat = []
+    self.stattitle = ["HP", "SP", "ATK", "DEF", "SPD", "MAG", "EVD"]
+    self.hp = self.stat.append(raceini.hp)
+    self.sp =  self.stat.append(raceini.sp)
+    self.atk =  self.stat.append(raceini.atk)
+    self.defn =  self.stat.append(raceini.defn)
+    self.spd =  self.stat.append(raceini.spd)
+    self.mag =  self.stat.append(raceini.mag)
+    self.evd =  self.stat.append(raceini.evd)
 
     self.capson = True
 
     self.name = []
-    self.capletters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', ' ']
-    self.letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' ']
 
     self.othercommands = ["Clear", "Delete", "Confirm"]
     self.background = self.engine.loadImage(os.path.join("Data", "characterbackground.png"))
@@ -62,64 +62,33 @@ class CharacterCreator(Layer):
     self.typingname = False
     self.racechooser = False
 
-    self.racebutton = self.engine.loadImage(os.path.join("Data", "mapmenubuttonactive.png"))
-    self.racebuttonactive = self.engine.loadImage(os.path.join("Data", "mapmenubutton.png"))
+    self.racebutton = self.engine.loadImage(os.path.join("Data", "mapmenubutton.png"))
+    self.racebuttonactive = self.engine.loadImage(os.path.join("Data", "mapmenubuttonactive.png"))
 
   def TypeName(self):
     self.engine.drawImage(os.path.join(self.background), scale = (640,480))
     name = string.join(self.name, '')
-    namefont = GameEngine.renderFont("default.ttf", name, (320 , 96), size = 32)
+    namefont = self.engine.renderFont("default.ttf", name, (320 , 96), size = 32)
 
-    for i, choice in enumerate(self.capletters):
-      if i <= 8:
-        button = self.engine.drawImage(self.textbutton, coord= (218 + (36*i) , 164), scale = (32,32))
-      if 9 <= i <= 17:
-        button = self.engine.drawImage(self.textbutton, coord= (218 + (36*(i-9)) , 200), scale = (32,32))
-      if 18 <= i <= 26:
-        button = self.engine.drawImage(self.textbutton, coord= (218 + (36*(i-18)) , 236), scale = (32,32))
+    for i in range(0,26):
+      button = self.engine.drawImage(self.textbutton, coord= (218 + (36*(i%9)) , 164+(36*(i/9))), scale = (32,32))
       active, flag = self.engine.mousecol(button)
       if active == True:
-        if i <= 8:
-          button = self.engine.drawImage(self.textbuttonactive, coord= (218 + (36*i) , 164), scale = (32,32))
-        if 9 <= i <= 17:
-          button = self.engine.drawImage(self.textbuttonactive, coord= (218 + (36*(i-9)) , 200), scale = (32,32))
-        if 18 <= i <= 26:
-          button = self.engine.drawImage(self.textbuttonactive, coord= (218 + (36*(i-18)) , 236), scale = (32,32))
+        button = self.engine.drawImage(self.textbuttonactive, coord= (218 + (36*(i%9)) , 164+(36*(i/9))), scale = (32,32))
         if flag == True:
-          self.name.append(self.capletters[i])
+          self.name.append(chr(97+i).upper())
 
-      if i <= 8:
-        buttonfont = GameEngine.renderFont("default.ttf", str(choice), (218 + (36*i) , 164), size = 24)
-      if 9 <= i <= 17:
-        buttonfont = GameEngine.renderFont("default.ttf", str(choice), (218 + (36*(i-9)) , 200), size = 24)
-      if 18 <= i <= 26:
-        buttonfont = GameEngine.renderFont("default.ttf", str(choice), (218 + (36*(i-18)) , 236), size = 24)
+      buttonfont = self.engine.renderFont("default.ttf", chr(97+i).upper(), (218 + (36*(i%9)) , 164+(36*(i/9))), size = 24)
 
-    for i, choice in enumerate(self.letters):
-      if i <= 8:
-        button = self.engine.drawImage(self.textbutton, coord= (218 + (36*i) , 272), scale = (32,32))
-      if 9 <= i <= 17:
-        button = self.engine.drawImage(self.textbutton, coord= (218 + (36*(i-9)) , 308), scale = (32,32))
-      if 18 <= i <= 26:
-        button = self.engine.drawImage(self.textbutton, coord= (218 + (36*(i-18)) , 344), scale = (32,32))
+      button = self.engine.drawImage(self.textbutton, coord= (218 + (36*(i%9)) , 272+(36*(i/9))), scale = (32,32))
       active, flag = self.engine.mousecol(button)
       if active == True:
-        if i <= 8:
-          button = self.engine.drawImage(self.textbuttonactive, coord= (218 + (36*i) , 272), scale = (32,32))
-        if 9 <= i <= 17:
-          button = self.engine.drawImage(self.textbuttonactive, coord= (218 + (36*(i-9)) , 308), scale = (32,32))
-        if 18 <= i <= 26:
-          button = self.engine.drawImage(self.textbuttonactive, coord= (218 + (36*(i-18)) , 344), scale = (32,32))
+        button = self.engine.drawImage(self.textbuttonactive, coord= (218 + (36*(i%9)) , 272+(36*(i/9))), scale = (32,32))
         if flag == True:
-          self.name.append(self.letters[i])
+          self.name.append(chr(97+i))
 
-      if i <= 8:
-        buttonfont = GameEngine.renderFont("default.ttf", str(choice), (218 + (36*i) , 272), size = 24)
-      if 9 <= i <= 17:
-        buttonfont = GameEngine.renderFont("default.ttf", str(choice), (218 + (36*(i-9)) , 308), size = 24)
-      if 18 <= i <= 26:
-        buttonfont = GameEngine.renderFont("default.ttf", str(choice), (218 + (36*(i-18)) , 344), size = 24)
-      
+      buttonfont = self.engine.renderFont("default.ttf", chr(97+i), (218 + (36*(i%9)) , 272+(36*(i/9))), size = 24)
+
     for i, choice in enumerate(self.othercommands):
       button = self.engine.drawImage(self.textbutton, coord= (90, 186 + (76*i)), scale = (150,44))
       active, flag = self.engine.mousecol(button)
@@ -136,7 +105,7 @@ class CharacterCreator(Layer):
           if i == 2:
             self.name = self.name
             self.typingname = False     
-      buttonfont = GameEngine.renderFont("default.ttf", str(choice), (90, 186 + (76*i)), size = 24)
+      buttonfont = self.engine.renderFont("default.ttf", str(choice), (90, 186 + (76*i)), size = 24)
 
 
   def drawRaceMenu(self):
@@ -152,14 +121,14 @@ class CharacterCreator(Layer):
     buttonfont = self.races
 
     for i, choice in enumerate(self.races):
-      button[i] = GameEngine.drawImage(self.racebutton, coord= (320, 64+(48*i)), scale = (200,32))
-      active, flag = GameEngine.mousecol(button[i])
+      button[i] = self.engine.drawImage(self.racebutton, coord= (320, 64+(48*i)), scale = (200,32))
+      active, flag = self.engine.mousecol(button[i])
       if active == True:
-        button[i] = GameEngine.drawImage(self.racebuttonactive, coord= (320, 64+(48*i)), scale = (200,32))
+        button[i] = self.engine.drawImage(self.racebuttonactive, coord= (320, 64+(48*i)), scale = (200,32))
       if flag == True:
         self.race = choice
         self.racechooser = False
-      buttonfont[i] = GameEngine.renderFont("menu.ttf", str(choice), (320, 64+(48*i)), size = 24)  
+      buttonfont[i] = self.engine.renderFont("menu.ttf", str(choice), (320, 64+(48*i)), size = 24)  
    
   def update(self):
     self.engine.drawImage(os.path.join(self.background), scale = (640,480))
@@ -175,14 +144,14 @@ class CharacterCreator(Layer):
         button = self.engine.drawImage(self.menubuttonactive, coord= (90, 130), scale = (150,45))
         if flag == True:
           self.typingname = True
-      buttonfont = GameEngine.renderFont("default.ttf", "Change Name", (90, 130))
+      buttonfont = self.engine.renderFont("default.ttf", "Change Name", (90, 130))
 
-      otherfont = GameEngine.renderFont("default.ttf", "Name", (200, 130), size = 24)
+      otherfont = self.engine.renderFont("default.ttf", "Name", (200, 130), size = 24)
 
       name = string.join(self.name, '')
-      namefont = GameEngine.renderFont("default.ttf", name, (380, 130), size = 32)
+      namefont = self.engine.renderFont("default.ttf", name, (380, 130), size = 32)
 
-      GameEngine.renderFont("default.ttf", "Create A Character", (170, 70), size = 24)
+      self.engine.renderFont("default.ttf", "Create A Character", (170, 70), size = 24)
 
       button = self.engine.drawImage(self.menubutton, coord= (540, 420), scale = (150,45))
       active, flag = self.engine.mousecol(button)
@@ -192,7 +161,7 @@ class CharacterCreator(Layer):
           #self.saveini(name)
           pass
 
-      namefont = GameEngine.renderFont("default.ttf", "Confirm", (540, 420))
+      namefont = self.engine.renderFont("default.ttf", "Confirm", (540, 420))
 
 
       button = self.engine.drawImage(self.menubutton, coord= (90, 190), scale = (150,45))
@@ -202,27 +171,14 @@ class CharacterCreator(Layer):
         if flag == True:
           self.racechooser = True
 
-      buttonfont = GameEngine.renderFont("default.ttf", "Change Race", (90, 190))
-      otherfont = GameEngine.renderFont("default.ttf", "Race", (200, 190), size = 24)
+      buttonfont = self.engine.renderFont("default.ttf", "Change Race", (90, 190))
+      otherfont = self.engine.renderFont("default.ttf", "Race", (200, 190), size = 24)
 
       name = self.race.split(".")
-      namefont = GameEngine.renderFont("default.ttf", name[0], (380, 190), size = 32)
+      namefont = self.engine.renderFont("default.ttf", name[0], (380, 190), size = 32)
 
-      GameEngine.renderFont("default.ttf", "HP", (100, 240), size = 24)
-      GameEngine.renderFont("default.ttf", "SP", (100, 272), size = 24)
-      GameEngine.renderFont("default.ttf", "ATK", (100, 304), size = 24)
-      GameEngine.renderFont("default.ttf", "DEF", (100, 336), size = 24)
-      GameEngine.renderFont("default.ttf", "SPD", (100, 368), size = 24)
-      GameEngine.renderFont("default.ttf", "EVD", (100, 400), size = 24)
-      GameEngine.renderFont("default.ttf", "MAG", (100, 432), size = 24)
+      for i, stat in enumerate(self.stat):
+        self.engine.renderFont("default.ttf", self.stattitle[i], (100, 240 + (i*32)), size = 24)
 
-
-      GameEngine.renderFont("default.ttf", self.hp, (280, 240), size = 24)
-      GameEngine.renderFont("default.ttf", self.sp, (280, 272), size = 24)
-      GameEngine.renderFont("default.ttf", self.atk, (280, 304), size = 24)
-      GameEngine.renderFont("default.ttf", self.defn, (280, 336), size = 24)
-      GameEngine.renderFont("default.ttf", self.spd, (280, 368), size = 24)
-      GameEngine.renderFont("default.ttf", self.evd, (280, 400), size = 24)
-      GameEngine.renderFont("default.ttf", self.mag, (280, 432), size = 24)
-
+        self.engine.renderFont("default.ttf", str(stat), (280, 240 + (i*32)), size = 24)
 
