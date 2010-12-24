61075221a126577469b94255ce4559f7a5b8ba9c
nhydock <blazingamer@blazingamer-laptop.(none)>
2010-12-23 20:21:05 -0500

    character .ini is now being chuncked for easier organization and reability. Party is now a class which will be handy for battle and future features.

--- src/Actor.py
+++ src/Actor.py
@@ -16,48 +16,7 @@ from math import *
 import random
 
 import glob
-
-class Job:
-    def __init__(self, name):
-        path = os.path.join("actors", "jobs", name)
-        jobini = Configuration(os.path.join("..", "data", path, "job.ini")).job
-        
-        self.name = name
-
-        #These stats are what the job starts out with
-        # characters can go against the basic stats for example
-        # you can create a mage that have high str instead of
-        # the default high mag, however, certain skills are learned
-        # not on level but on how many stat points are distributed
-        # into what categories
-        self.description = jobini.__getattr__("description")
-        self.hp   = jobini.__getattr__("hp",  int)
-        self.str  = jobini.__getattr__("str", int)
-        self.defn = jobini.__getattr__("def", int)
-        self.spd  = jobini.__getattr__("spd", int)
-        self.evd  = jobini.__getattr__("evd", int)
-        self.mag  = jobini.__getattr__("mag", int)
-        self.res  = jobini.__getattr__("res", int)
-        
-        self.stats = [self.hp, self.str, self.defn, self.spd, self.evd, self.mag, self.res]
-        
-        self.sprites = [ImgObj(Texture(os.path.join(path, sprite.rsplit("/",1)[1])))
-                        for sprite in glob.glob(os.path.join(path, "*.png"))]
-
-        #this stat is an equation in terms of x with w being the
-        # character's level.  It determines the maximum amount
-        # of fighting power the character has.
-        #self.fightPT  = jobini.__getattr__("fightCurve")
-
-    def drawStatGraph(self):
-        glBegin(GL_LINE_STRIP)
-        glColor3f(1.0, 0.0, 0.0); glVertex2f(-self.stats[0],   0)
-        glColor3f(1.0, 1.0, 0.0); glVertex2f(-self.stats[1]/2,  self.stats[1]/2)
-        glColor3f(1.0, 0.5, 0.0); glVertex2f( self.stats[2]/2,  self.stats[2]/2)
-        glColor3f(1.0, 0.0, 1.0); glVertex2f( self.stats[3],   0)
-        glColor3f(0.0, 0.5, 1.0); glVertex2f( self.stats[4]/2, -self.stats[4]/2)
-        glColor3f(0.0, 1.0, 0.0); glVertex2f(-self.stats[5]/2, -self.stats[5]/2)
-        glEnd()
+import Jobs
 
 class Character:
     _LevelMax = 20      #this is the current level cap, I will adjust this with the number of content available
@@ -74,29 +33,36 @@ class Character:
         if not name:
             return
 
-        playerini = Configuration(os.path.join("..", "data", "actors", "families", family, name + ".ini")).character
-        self.job  = Job(playerini.__getattr__("job"))
+        playerini = Configuration(os.path.join("..", "data", "actors", "families", family, name + ".ini"))
+        
+        #divides the character ini into resonable chunks
+        baseSection  = playerini.character      #contains basic information about the character
+        distSection  = playerini.distribution   #contains stat distribution information
+        equipSection = playerini.equipment      #contains all the equipment being worn
+        profSection  = playerini.proficency     #contains information about the character's weapon proficency
+        
+        self.job  = eval(baseSection.__getattr__("job")+"()")
 
-        self.level      = playerini.__getattr__("level", int)
-        self.exp        = playerini.__getattr__("exp",   int)
+        self.level      = baseSection.__getattr__("level", int)
+        self.exp        = baseSection.__getattr__("exp",   int)
 
         #the amount of points added to each field
         # these are set upon the creation of the character
         # and upon leven up when the bonus stat points
         # are distributed
-        self.hpDist  = playerini.__getattr__("hpDist",  int)
-        self.strDist = playerini.__getattr__("strDist", int)
-        self.defDist = playerini.__getattr__("defDist", int)
-        self.spdDist = playerini.__getattr__("spdDist", int)
-        self.evdDist = playerini.__getattr__("evdDist", int)
-        self.magDist = playerini.__getattr__("magDist", int)
-        self.resDist = playerini.__getattr__("resDist", int)
+        self.hpDist  = distSection.__getattr__("hpDist",  int)
+        self.strDist = distSection.__getattr__("strDist", int)
+        self.defDist = distSection.__getattr__("defDist", int)
+        self.spdDist = distSection.__getattr__("spdDist", int)
+        self.evdDist = distSection.__getattr__("evdDist", int)
+        self.magDist = distSection.__getattr__("magDist", int)
+        self.resDist = distSection.__getattr__("resDist", int)
 
         #these are the points the character has that are open for
         #distribution amongst his stats
-        self.points = playerini.__getattr__("points", int)
+        self.points = baseSection.__getattr__("points", int)
 
-        self.currentHp = playerini.__getattr__("currenthp", int)
+        self.currentHp = baseSection.__getattr__("currenthp", int)
 
         self.hp   = self.job.hp   + hpDist,   #hit points
         self.str  = self.job.str  + strDist,  #strength
@@ -106,8 +72,41 @@ class Character:
         self.mag  = self.job.mag  + magDist,  #magic strength
         self.res  = self.job.res  + resDist   #magic defense
 
+        #any character can weild any weapon, to balance things there is a proficency system
+        #for equipment.  Every 100 points raises the level of the proficency.  
+        self.swordProf = self.job.swordProf + profSection.__getattr__("sword", int)
+        self.daggerProf = self.job.daggerProf + profSection.__getattr__("dagger", int)
+        self.spearProf = self.job.spearProf + profSection.__getattr__("spear", int)
+        self.staffProf = self.job.staffProf + profSection.__getattr__("staff", int)
+        self.gunsProf = self.job.gunsProf + profSection.__getattr__("guns", int)
+        
+        #there are 10 pieces of equipment one can wear
+        #left hand weapon, right hand weapon, helm, armor, legs, feet, gloves, and 3 accessories
+        self.equipment = [equipSection.__getattr__("left hand"),
+                          equipSection.__getattr__("right hand"),
+                          equipSection.__getattr__("helmet"),
+                          equipSection.__getattr__("armor"), 
+                          equipSection.__getattr__("legs"), 
+                          equipSection.__getattr__("feet"),
+                          equipSection.__getattr__("gloves"),
+                          equipSection.__getattr__("accessory1"),
+                          equipSection.__getattr__("accessory2"),
+                          equipSection.__getattr__("accessory3")]
+        
+        self.hand = 0       #0 = right handed, 1 = left handed
+                            #dominant hand determines which weapons gets 
+                            #proficency points and damage boost in battle
+        
         x = self.level
         self.maxFP = int(eval(self.job.fightPT))
+        
+        #is the character attacking
+        self.attack = False
+        
+        #are they casting a spell or technique, if they are, which one
+        self.cast = False
+        self.command = None
+        
         #when a character boosts instead of defends,
         #their def is halved but they get full FP the next turn
         self.boost = False
@@ -128,7 +127,8 @@ class Character:
             self.defn /= 2
         elif self.defend:
             self.defn *= 2.5
-        
+        elif self.attack:
+            self.damage = self.str + self.equipment[0].str + (self.str*
     def turnEnd(self):
         self.fp += self.maxFP / 5
 
@@ -248,3 +248,22 @@ class Family:
     def refresh(self):
         self.__init__(self.name)
 
+#party for battle has a few specifics for conditions
+#depending on members present in it
+class Party:
+    def __init__(self, members):
+        self.members = members
+        
+        
+        self.shepardPresent = False
+        self.treasureHunterPresent = False
+        self.piratePresent = False
+        for mem in self.members:
+            if isInstance(mem, Shepard):
+                self.shepardPresent = True
+            elif isInstance(mem, TreasureHunter):
+                self.treasureHunterPresent = True
+            elif isInstance(mem, Pirate):
+                self.piratePresent = True
+            
+        
