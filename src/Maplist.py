fac62ffc60e86bd82e8ea3f5c3023631beb22473
nhydock <n_hydock@comcast.net>
2011-01-01 16:47:21 -0500

    Some work done on the MapList

--- src/Maplist.py
+++ src/Maplist.py
@@ -1,124 +1,148 @@
-#=======================================================#
-#
-# UlDunAd - Ultimate Dungeon Adventure
-# Copyright (C) 2009 Blazingamer/n_hydock@comcast.net
-#       http://code.google.com/p/uldunad/
-# Licensed under the GNU General Public License V3
-#      http://www.gnu.org/licenses/gpl.html
-#
-#=======================================================#
+'''
 
-import Engine
-from Engine import GameEngine
+2010 Nicholas Hydock
+UlDunAd
+Ultimate Dungeon Adventure
 
-from View import *
+Licensed under the GNU General Public License V3
+     http://www.gnu.org/licenses/gpl.html
 
-import random
+'''
 
-from Config import Configuration
+from sysobj import *
+from View   import *
+from Actor  import *
 
+import string
 import Input
 
-class Maplist(Layer):
-  def __init__(self):
-    self.engine = GameEngine()
-    self.background = self.engine.loadImage(os.path.join("Data", "Interface", "mapbackground.png"))
+from MenuObj import MenuObj
 
-    self.button = self.engine.data.mapbutton
-    self.menubutton = self.engine.data.secondarybutton
+class MapList(Scene):
+    def __init__(self, engine):
+        self.engine = engine
 
-    self.maps = []
-    self.towns = self.engine.listpath(os.path.join("Data", "Places", "Towns"), "searchfile", "town.ini")
-    self.dungeons = self.engine.listpath(os.path.join("Data", "Places", "Dungeons"), "searchfile", "dungeon.ini")
-    self.maps.extend(self.towns), self.maps.extend(self.dungeons)
+        scenepath = os.path.join("scenes", "maplist")
+        self.background = ImgObj(Texture(os.path.join(scenepath, "background.png")))
+        self.window = WinObj(Texture(os.path.join(scenepath, "window.png")), self.engine.w/4, 0)
+        self.button = ImgObj(Texture("ok.png"), boundable = True, frameX = 2)
+        self.font   = FontObj("default.ttf")
 
-    self.formations = self.engine.listpath(os.path.join("Data", "Actors", "Enemies", "Formations"), "splitfiletype", ".ini")
-    self.mapinfo = self.engine.makeWindow(scale = (310, 356))
+        self.maps   = self.engine.listPath("places")
+        self.startIndex = 0
+        self.endIndex = min(10, len(self.maps))
+        
+        commands = ["Up"]
+        for i in range(self.startIndex, self.endIndex):
+            commands.append(self.maps[i])
+        commands.append("Down")
+        self.menu   = MenuObj(self, commands, position = (100, 400))
+        
+        #family info
+        self.name = []          #name of the family
+        self.diffselected = 1   #the difficulty selected (match up number with position in difficulty array
+                                # (1 = default, Normal difficulty)
 
-    self.initmenu = self.engine.createMenu(self.engine.data.menuWindow, self.engine.data.menuwindowbutton, ["Show Maps", "Show Menu", "Random Battle"], (112, 126), 160)
-    self.index = 0
-    self.showmaps = False
-    self.selectedmap = None
+        self.fadeIn     = True  #are the windows transitioning in or out
 
-    if self.formations != []:
-      self.formation = random.choice(self.formations)
+        self.error = False      #was an error thrown
+        self.step = 0           #step 0 = naming, step 1 = choose difficulty
+        
+        self.exists = False
 
-  def update(self):
-    self.engine.drawImage(self.background, scale = (640,480))
+    def buttonClicked(self, image):
+        pass
+        
+    def keyPressed(self, key, char):    
+        self.menu.keyPressed(key)                
 
-    if self.showmaps == True:
-      if self.index < 0:
-        self.index = 0
-      if self.index > len(self.maps):
-        self.index = len(self.maps) - 10
-
-      maxindex = len(self.maps)
-      for i in range(self.index, 10+self.index):
-        if i < maxindex:
-          active, flag = self.engine.drawButton(self.button, coord= (127, 70 + (36*(i+1))), scale = (214,36))
-          if flag == True:
-            self.selectedmap = self.maps[i]
-          
-          buttonfont = self.engine.renderFont("default.ttf", self.maps[i], (70, 70+(36*(i+1))), size = 16, flags = "Shadow", alignment = 1)
-
-      self.engine.drawWindow(self.mapinfo, coord = (480, 220))
-  
-      if self.selectedmap != None:
-        self.engine.renderFont("default.ttf", self.selectedmap, (480, 70), size = 24, flags = "Shadow")
-        self.engine.renderFont("default.ttf", "Location Type:", (480, 300), size = 20, flags = "Shadow")
-
-        if self.selectedmap in self.dungeons:
-          loctype = "Dungeon"
-          mapback = self.engine.loadImage(os.path.join("Data", "Places", "Dungeons", self.selectedmap, "background.png"))
-          self.engine.drawImage(mapback, coord = (480, 190), scale = (260,195))
+    def select(self, index):
+        #if Up is selected
+        if index == 0:
+            self.startIndex = max(self.startIndex - 10, 0)
+        #if Down is selected
+        elif index == len(self.menu.commands)-1:
+            self.startIndex = min(self.startIndex+10, len(self.maps)-11)
+            
+                
+        #if Up or Down is selected, the end index will be corrected as well
+        if index == 0 or index == len(self.menu.commands)-1:
+            #endIndex will always be 10 ahead of startIndex
+            self.endIndex = min(self.startIndex + 10, len(self.maps))
+            
+            self.updateCommands()
+            
+        if index > 0 and index < len(self.menu.commands)-1:
+            self.selectedMap = self.maps[self.startIndex + index]
+      
+    #updates the list of maps
+    def updateCommands(self):
+        commands = ["Up"]
+        for i in range(self.startIndex, self.endIndex):
+            commands.append(self.maps[i])
+        commands.append("Down")
+        
+        self.menu.commands = commands
+        
+    def run(self):
+        pass
+        
+    def next(self):
+        if self.step == 0 and not self.name:
+            self.error = True
         else:
-          loctype = "Town"
-          mapback = self.engine.loadImage(os.path.join("Data", "Places", "Towns", self.selectedmap, "background.png"))
-          self.engine.drawImage(mapback, coord = (480, 190), scale = (260,195))
-        self.engine.renderFont("default.ttf", loctype, (480, 320), size = 20, flags = "Shadow")
+            self.step += 1
+
+    def renderNaming(self):
+        w, h = self.engine.w, self.engine.h
+        self.window.setPosition(w/2, h/2)
+        self.window.setDimensions(w*.4, h*.15)
+        self.window.draw()
+        
+        if self.window.scale[0] == w*.4 and self.window.scale[1] == h*.15:
+            self.engine.drawText(self.font, "Enter a name", (w*.5, h*.6))
+            name = string.join(self.name, '')
+            self.engine.drawText(self.font, name, (w*.5, h*.5))
+
+            if name:
+                if self.exists:
+                    frame = 2
+                else:
+                    frame = 1
+                self.engine.drawImage(self.button, position = (w*.25, h*.5), scale = (75,75), frameX = frame)
+            
+                
+    def renderDifficulty(self):
+        w, h = self.engine.w, self.engine.h
+        self.window.setPosition(w/2,h/2)
+        self.window.setDimensions(w*.65, h*.25)
+        self.window.draw()
+
+        if self.window.scale[0] >= w*.65 and self.window.scale[1] >= h*.20:
+            self.engine.drawText(self.font, "Select the difficulty", (w*.5, h*.65))
+            self.menu.render()
+            
+    def create(self):
+        name = string.join(self.name, '')
+        family = Family(None)
+        family.create(name, self.diffselected)
+        self.engine.family = Family(name)
+        self.engine.viewport.changeScene("CreateCharacter")
+        
+    def render(self, visibility):
+        w, h = self.engine.w, self.engine.h
+
+        self.engine.drawImage(self.background, scale = (w,h))
+
+        if self.step == 0:
+            self.renderNaming()
+        elif self.step == 1:
+            self.renderDifficulty()
+        else:
+            self.create()
+
+        if self.error:
+            self.engine.showError("You must enter a name")
+
 
         
-        active, flag = self.engine.drawButton(self.menubutton, coord= (480, 350), scale = (214,36))
-        if flag == True:
-          Engine.town = self.selectedmap
-          if loctype == "Dungeon":
-            from Dungeon import Dungeon
-            self.engine.changescene(self, Dungeon())
-          else:
-            from Towns import Towns
-            self.engine.changescene(self, Towns())
-
-        buttonfont = self.engine.renderFont("default.ttf", "Explore", (480, 350), size = 16, flags = "Shadow")
-
-      active, flag = self.engine.drawButton(self.button, coord= (127, 70), scale = (214,36))
-      if flag == True:
-        if self.index + 10 < maxindex:
-          self.index += 10
-      buttonfont = self.engine.renderFont("default.ttf", "-UP-", (70, 70), size = 16, alignment = 1, flags = "Shadow")
-
-      active, flag = self.engine.drawButton(self.button, coord= (127, 430), scale = (214,36))
-      if flag == True:
-        if self.index - 10 >= 0:
-          self.index -= 10
-      buttonfont = self.engine.renderFont("default.ttf", "-DOWN-", (70, 430), size = 16, alignment = 1, flags = "Shadow")
-
-      active, flag = self.engine.drawButton(self.menubutton, coord= (520, 430), scale = (214,36))
-      if flag == True:
-        self.selectedmap = None
-        self.showmaps = False
-      buttonfont = self.engine.renderFont("default.ttf", "Hide Maps", (520, 430), size = 16, flags = "Shadow")
-
-    else:
-      buttons = self.engine.drawMenu(self.initmenu)
-      if buttons[0][1] == True:
-        self.showmaps = True
-      elif buttons[1][1] == True:
-        from MenuSystem import MenuSystem
-        self.engine.changescene(self, MenuSystem())
-      elif buttons[2][1] == True:
-        if self.formations != []:
-          from BattleScene import BattleScene 
-          self.engine.changescene(self, BattleScene(str(self.formation)))
-          from ExtraScenes import LoadingScene
-          View().addscene(LoadingScene("Preparing Battle", 4.5))
-
