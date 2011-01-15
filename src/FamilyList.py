7e908e03de059cf5ae975e64ddc953413564af70
nhydock <n_hydock@comcast.net>
2011-01-15 09:27:18 -0500

    Did some massive work on the FamilyList within the past hour. The scene is taking shape and the menu is working. Still needs a bit of touching up.

--- src/FamilyList.py
+++ src/FamilyList.py
@@ -3,43 +3,90 @@ from View   import *
 from Actor  import *
 from MenuObj import MenuObj
 
+import Input
+
 class FamilyMenu(MenuObj):
     def __init__(self, scene, families):
         self.scene = scene
         self.engine = scene.engine
         self.commands = families
+        self.moveKeys = [Input.DButton, Input.UButton]
+        
+        scenepath = os.path.join("scenes", "familylist")
         
         fontStyle = self.engine.data.defaultFont
         self.text     = FontObj(fontStyle)
         
-        buttonStyle  = Texture("select_highlight.png")
+        buttonStyle  = Texture(os.path.join(scenepath, "highlight.png"))
         self.buttons = [ImgObj(buttonStyle, boundable = True, frameY = 2)
-                        for n in range(len(self.families))]
+                        for n in range(len(self.commands))]
                         
-        self.position = (20, 10)
+        self.position = (self.engine.w/2, self.engine.h)
         for i, button in enumerate(self.buttons):
             button.setPosition(self.position[0], self.position[1] - (150 * i))
         self.slideRate = 150.0/512.0
         
         self.index = 0
+        self.startIndex = 0
+        self.endIndex = min(4, len(self.commands))
+
+    def keyPressed(self, key):
+        MenuObj.keyPressed(self, key)
         
-    def render(self, visibility):
-        
+        #if Up is selected
+        if self.index == self.startIndex and key == Input.UButton:
+            if len(self.commands) > 0:
+                self.startIndex = max(self.startIndex - 4, 0)
+        #if Down is selected
+        elif self.index == self.endIndex and key == Input.DButton:
+            if len(self.commands) > 4:
+                self.startIndex = min(self.startIndex+4, len(self.commands)-5)
+        self.endIndex = min(self.startIndex + 4, len(self.commands))
+
+    
+    #renders the menu
+    def render(self, visibility = 1.0):
+
+        for i, button in enumerate(self.buttons):
+            if i == self.index:
+                button.setFrame(y = 2)
+            else:
+                button.setFrame(y = 1)
+            button.draw()
+            
+        for i, family in enumerate(self.commands[self.startIndex:self.endIndex]):
+            self.text.setText(family.name) 
+            self.text.setPosition(button.position[0], button.position[1])
+            self.text.scaleHeight(36.0)
+            self.text.draw()
+            
+            self.text.setText("Members:%i" % len(family.members)) 
+            self.text.setPosition(button.position[0] + button.width, button.position[1])
+            self.text.scaleHeight(36.0)
+            self.text.setAlignment("right")
+            self.text.draw()
+            
+            
 class FamilyList(Scene):
     def __init__(self, engine):
         self.engine     = engine
-        self.families   = self.engine.listPath(path = os.path.join("data", "actors", "families"))
+        self.families   = [Family(n) for n in self.engine.listPath(path = os.path.join("actors", "families"), 
+                                                                   value = "family.ini", flag = "folderDeepSearch")]
 
-        self.background = ImgObj(Texture("select_background.png"))
+        self.menu = FamilyMenu(self, self.families)
         
-        self.hlTex      = Texture("select_highlight.png")
+        scenepath = os.path.join("scenes", "familylist")
+        self.background = ImgObj(Texture(os.path.join(scenepath, "background.png")))
+        
+        self.hlTex      = Texture(os.path.join(scenepath, "highlight.png"))
         self.highlight  = [ImgObj(self.hlTex, boundable = True, frameY = 2)
-                           for n in range(len(self.families))]
+                           for n in range(4)]
         #self.slider     = ImgObj(Texture("slider.png"))
-        self.slide_b    = [ImgObj(Texture("slider_buttons.png"), boundable = True, frameY = 2),
-                           ImgObj(Texture("slider_buttons.png"), boundable = True, frameY = 2)]
-        self.selectwin  = ImgObj(Texture("window_selected.png"))
-
+        self.slide_b    = [ImgObj(Texture(os.path.join(scenepath, "buttons.png")), boundable = True, frameX = 2),
+                           ImgObj(Texture(os.path.join(scenepath, "buttons.png")), boundable = True, frameX = 2)]
+        self.selectwin  = WinObj(Texture(os.path.join(scenepath, "window.png")), 0, self.engine.h-200)
+        self.selectwin.setDimensions(self.engine.w, 200)
+        
         self.pos        = 0
         self.newpos     = 0
         self.slideUP    = False
@@ -48,74 +95,26 @@ class FamilyList(Scene):
         self.listlength = self.engine.h*(self.hlTex.pixelSize[1]/2*len(self.highlight))
         
         self.selected   = 0
-
-    def buttonClicked(self, image):
-        if Scene.objInput == self.slide_b[0]:
-            if self.pos > 0:
-                self.slideUP = True
-                self.newPos = self.pos - 30
-                if self.newPos < 0:
-                    self.newPos = 0
-        elif Scene.objInput == self.slide_b[1]:
-            if self.pos < self.listlength:
-                self.slideUP = True
-                self.newPos = self.pos + 30
-                if self.newPos > self.listlength:
-                    self.newPos = self.listlength
-
-        elif Scene.objInput in self.highlight:
-            self.selected = Scene.objInput
     
     def keyPressed(self, key, char):
-        if key == K_DOWN:
-            if self.selected < len(self.families):
-                self.selected += 1
-            else:
-                self.selected = 0
-                
-        if key == K_UP:
-            if self.selected > 0:
-                self.selected -= 1
-            else:
-                self.selected = len(self.families) - 1
-                 
+        self.menu.keyPressed(key)
+      
+    def select(self, index):
+        self.engine.family = self.families[index]
+        self.engine.viewport.changeScene("MapList")
+        
     def run(self):
-
-        #this will slide the list up or down in a smooth motion
-        #when one of the slide buttons is clicked!  Each click
-        #will offset the list by 30 px and clicking can only be
-        #either up or down.  Input can not be taken while the list
-        #is sliding to its new position.
-
-        if self.slideUP:
-            if self.pos > self.newPos:
-                self.pos -= 1
-            else:
-                self.pos = self.newPos
-                self.slideUP = False
-
-        if self.slideDOWN:
-            if self.pos < self.newPos:
-                self.pos += 1
-            else:
-                self.pos = self.newPos
-                self.slideDOWN = False
+        pass
 
     def render(self, visibility):
         w, h = self.engine.w, self.engine.h
 
         self.engine.drawImage(self.background, scale = (w, h))
 
-        self.engine.drawImage(self.selectwin, position = (w/2, h*.9), scale = (w, h*.2))
-            
-        for f, i in enumerate(self.highlight):
-            frame = 1
-            if f == self.selected:
-                frame = 2
-            self.engine.drawImage(f, position = (w/2, self.pos + self.listlength/len(self.highlight)*i), frameY = frame)
-            
+        self.selectwin.draw()
 
-        self.engine.drawImage(self.slide_b[0], position = (w*.9, h*.1), frameY = 1)
-        self.engine.drawImage(self.slide_b[1], position = (w*.9, h*.9), frameY = 2) 
+        self.engine.drawImage(self.slide_b[0], position = (w*.9, h*.1), frameX = 1)
+        self.engine.drawImage(self.slide_b[1], position = (w*.9, h*.9), frameX = 2) 
 
+        self.menu.render(visibility)
         
