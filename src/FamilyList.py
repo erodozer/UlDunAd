b19e1646821b212c0c8d09fa30cf5352f1279e83
nhydock <n_hydock@comcast.net>
2011-01-17 08:31:03 -0500

    Mostly completed the FamilyList, all that can be done is layout reworking.

--- src/FamilyList.py
+++ src/FamilyList.py
@@ -21,9 +21,11 @@ class FamilyMenu(MenuObj):
         self.buttons = [ImgObj(buttonStyle, boundable = True, frameY = 2)
                         for n in range(len(self.commands))]
                         
-        self.position = (self.engine.w/2, self.engine.h)
+        self.position = (self.engine.w/10, self.engine.h - self.buttons[0].height)
         for i, button in enumerate(self.buttons):
-            button.setPosition(self.position[0], self.position[1] - (150 * i))
+            button.setScale(self.engine.w*.80, self.engine.h/6)
+            button.setPosition(self.position[0], self.position[1] - ((button.height + 10) * i))
+            button.setAlignment("left")
         self.slideRate = 150.0/512.0
         
         self.index = 0
@@ -31,37 +33,54 @@ class FamilyMenu(MenuObj):
         self.endIndex = min(4, len(self.commands))
 
     def keyPressed(self, key):
-        MenuObj.keyPressed(self, key)
-        
-        #if Up is selected
-        if self.index == self.startIndex and key == Input.UButton:
-            if len(self.commands) > 0:
-                self.startIndex = max(self.startIndex - 4, 0)
-        #if Down is selected
-        elif self.index == self.endIndex and key == Input.DButton:
-            if len(self.commands) > 4:
-                self.startIndex = min(self.startIndex+4, len(self.commands)-5)
+        if key == Input.AButton:
+            self.scene.select(self.index)
+            
+        if key == self.moveKeys[0]:
+            if self.index == self.endIndex-1:
+                if len(self.commands) > 4:
+                    self.startIndex = min(self.startIndex+4, len(self.commands)-4)
+            else:
+                if self.index + 1 < len(self.commands):
+                    self.index += 1
+                else:
+                    self.index = 0
+                
+        elif key == self.moveKeys[1]:
+            if self.index == self.startIndex-1:
+                if len(self.commands) > 0:
+                    self.startIndex = max(self.startIndex - 4, 0)
+            else:
+                if self.index > 0:
+                    self.index -= 1
+                else:
+                    self.index = len(self.commands) - 1        
+            
         self.endIndex = min(self.startIndex + 4, len(self.commands))
 
     
     #renders the menu
     def render(self, visibility = 1.0):
 
-        for i, button in enumerate(self.buttons):
+        families = self.commands[self.startIndex:self.endIndex]
+        for i, button in enumerate(self.buttons[:(self.endIndex-self.startIndex)]):
+            family = families[i]
+        
             if i == self.index:
                 button.setFrame(y = 2)
             else:
                 button.setFrame(y = 1)
+        
             button.draw()
-            
-        for i, family in enumerate(self.commands[self.startIndex:self.endIndex]):
+        
             self.text.setText(family.name) 
-            self.text.setPosition(button.position[0], button.position[1])
+            self.text.setPosition(button.position[0] - self.engine.w/10, button.position[1])
             self.text.scaleHeight(36.0)
+            self.text.setAlignment("left")
             self.text.draw()
             
+            self.text.setPosition(button.position[0] + button.width - self.engine.w/10, button.position[1])
             self.text.setText("Members:%i" % len(family.members)) 
-            self.text.setPosition(button.position[0] + button.width, button.position[1])
             self.text.scaleHeight(36.0)
             self.text.setAlignment("right")
             self.text.draw()
@@ -101,7 +120,7 @@ class FamilyList(Scene):
       
     def select(self, index):
         self.engine.family = self.families[index]
-        self.engine.viewport.changeScene("MapList")
+        self.engine.viewport.changeScene("Maplist")
         
     def run(self):
         pass
