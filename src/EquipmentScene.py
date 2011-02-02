3089003ed9707d4fc9193719a1bb0d831d9edaa9
nhydock <n_hydock@comcast.net>
2011-02-02 12:58:53 -0500

    More work on the equipment scene

--- src/EquipmentScene.py
+++ src/EquipmentScene.py
@@ -20,9 +20,12 @@ class EquipmentButtons(MenuObj):
     def __init__(self, scene):
         self.scene = scene
         self.engine = scene.engine
+        self.character = scene.character
         
         scenepath = os.path.join("scenes", "menusystem", "equipment")
         self.buttonTex = Texture(os.path.join(scenepath, "icon.png"))
+        self.window = WinObj(os.path.join(scenepath, "window.png"), width = self.engine.w/2,
+                             height = self.engine.h/2 - 64)
         
         #these buttons are placed up along the right side of the character image
         self.commandsV = ["weapon", "weapon", "accessory", "accessory", "accessory"]
@@ -47,20 +50,27 @@ class EquipmentButtons(MenuObj):
             self.activeArea = 0
             return True
         if image in self.commandButtonsV:
-            self.index = self.commandButtonsH.index(image)
+            self.index = self.commandButtonsV.index(image) + len(self.commandButtonsH)
             self.activeArea = 1
             return True
         return False
     
     def keyPressed(self, key, char):
+        pass
+        
+    def render(self, visibility):
         
 class EquipmentScene(Scene):
     def __init__(self, engine):
         self.engine = engine
+        self.family = self.engine.family
         
-        self.step = 0   #0 = equipment selection, 1 = item selection for replacement
+        self.activeChar = 0 #the character being displayed
+        self.activeCat = -1 #the active weapon category
         
+        self.step = 0       #0 = equipment selection, 1 = item selection for replacement
         
+
     def buttonClicked(self, image):
         if self.EquipMenu.buttonClicked(image):
             self.step = 0
@@ -70,19 +80,32 @@ class EquipmentScene(Scene):
     def keyPressed(self, key, char):
         if self.step == 0:
             self.EquipMenu.keyPressed(key)
+            if key == Input.BButton:
+                self.engine.viewport.changeScene("MenuSystem")
         else:
             self.ItemMenu.keyPressed(key)
+            if key == Input.BButton:
+                self.step = 1
+            
+        if key == Input.CButton:
+            self.activeChar += 1
+            self.refresh()
+        elif key == Input.DButton:
+            self.activeChar -= 1
+            self.refresh()
+
+    #refreshes the character info
+    def refresh(self):
+        if self.activeChar < 0:
+            self.activeChar = len(self.family.members) - 1
+        elif self.activeChar >= len(self.family.members):
+            self.activeChar = 0
+            
+        self.character = self.family.members[self.activeChar]
         
     def select(self, index):
-        if index == 0:
-            self.engine.viewport.changeScene("CreateFamily")
-        elif index == 1:
-            #forcing for testing purposes
-            #self.engine.family = Family("default")
-            #self.engine.viewport.changeScene("MapList")
-            self.engine.viewport.changeScene("FamilyList")
-        else:
-            self.engine.finished = True
+        if step == 0:
+            self.step = 
         
     def run(self):
         pass
