7b4614577653dd6afd6ce1e160e2d9f65fd576d8
nhydock <n_hydock@comcast.net>
2011-05-31 13:41:30 -0400

    Fixes timer display and button display

--- src/scenes/BattleSystem/BattleHUDAddition.py
+++ src/scenes/BattleSystem/BattleHUDAddition.py
@@ -9,18 +9,13 @@ class BattleHUDAddition:
         
         self.font = FontObj("default.ttf", 16)  #used for displaying the time
         
-        self.comboIndex = 0
-        self.comboTimer = self.weapon.time      #time given to perform the attack
-        self.addition = self.weapon.attack      #the list of input keys that need to be hit
-        self.success = -1                       #0 is failure, 1 = success
-        
         self.combo = self.character.command
         self.end = False
         
         #images used for displaying the input combo 
         self.inputButtons = [ImgObj(Texture("inputButtons.png"), frameY = 5) for i in self.combo.keys]
         for i, button in enumerate(self.inputButtons):
-            frame, angle = self.getButtonImage(self.addition[i])
+            frame, angle = self.getButtonImage(self.combo.keys[i])
             button.setFrame(y = frame)
             button.setAngle(angle)
             button.setScale(96,96, inPixels = True)
@@ -59,17 +54,17 @@ class BattleHUDAddition:
         
     def run(self):
         if self.combo.runTimer(self.engine.clock.get_time()):
-           self.end = True
+            self.end = True
                 
     def draw(self):
         y = self.engine.h/2
         for i in range(len(self.combo.keys)):
-            x = self.engine.w/2 - (self.inputButtons[0].width + 10.0) * (i - self.combo.keyIndex)
+            x = self.engine.w/2 - (self.inputButtons[0].width + 10.0) * (self.combo.keyIndex - i)
             if i == self.combo.keyIndex:
                 col = (1,1,1,1)
             else:
-                col = (1,1,1,.5)
+                col = (.5,1,1,.5)
             
             self.engine.drawImage(self.inputButtons[i], position = (x, y), color = col)
         
-        self.engine.drawText(self.font, "%3d" % self.comboTimer, position = (self.engine.w/2, self.engine.h/2 - 100))
+        self.engine.drawText(self.font, "%3d" % self.combo.timer, position = (self.engine.w/2, self.engine.h/2 - 100))
