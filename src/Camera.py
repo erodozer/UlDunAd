edb040906e69e34378361fecb367ef5bf794e7be
nhydock <n_hydock@comcast.net>
2011-08-03 14:19:52 -0400

    Returns camera to its old function

--- src/Camera.py
+++ src/Camera.py
@@ -18,60 +18,26 @@ from OpenGL.GLU import *
 #it starts out with a view with perspective, 
 #but you can make it orthographic with one call
 class Camera:
-    def __init__(self,pos,center,upvec=[0,1,0]):
-        #camera's values
-        self.pos = pos                  #current position
-        self.center = center            #current center
-        self.upvec = upvec              #current up vector
+    def __init__(self, pos, zoom = 100):
+
+        self.focusx = pos[0]
+        self.focusy = pos[1]
+        self.zoom = zoom
         
-        #values to which the camera wants to obtain
-        self.targetpos = pos            #new position
-        self.targetcenter = center      #new center
-        self.targetupvec = upvec        #new up vector
-        
-        
-        self.adjustRate = 0.1           #speed at which the camera will transition into 
-                                        # it's target values
-        
-    #sets the camera's target position
-    def setTargetPos(self, tP):
-        self.target = tP
-        
-    #sets the camera's target center
-    def setTargetCenter(self,tC):
-        self.targetcenter = tC
-        
-    #sets the camera's target up vector
-    def set_target_up_vector(self,new_target_up_vector):
-        self.targetupvec = new_target_up_vector
-       
-    #adjusts the change rate of the camera
-    def setAdjustRate(self,value):
-        self.adjust = value
-       
-    #updates the camera's values
-    def update(self):
-        #gets differences
-        diff = lambda i, n: [i[x] - n[x] for x in range(3)]  
-        
-        pos_diff = diff(self.pos, self.targetpos)
-        cen_diff = diff(self.center, self.targetcenter)
-        upv_diff = diff(self.upvec, self.targetupvec)
+        self._oldfocusx = self.focusx
+        self._oldfocusy = self.focusy
+        self._oldzoom = self.zoom
               
-        #gets the adjusted values
-        adjust = lambda i, diff: [i[x]-(diff[x]*self.adjustRate) for x in range(3)]
+    def focus(self, x, y, zoom):
+        self._oldfocusx = self.focusx
+        self._oldfocusy = self.focusy
+        self._oldzoom = self.zoom
         
-        self.pos = adjust(self.pos, pos_diff)
-        self.center = adjust(self.center, cen_diff)
-        self.upvec = adjust(self.upvec, upv_diff)
-       
-        self.setCamera()
-        
-    #sets up the camera to show what it's looking at
-    def setCamera(self):
-        gluLookAt(self.pos[0],self.pos[1],self.pos[2],
-                  self.center[0],self.center[1],self.center[2],
-                  self.upvec[0],self.upvec[1],self.upvec[2])
+        self.focusx = x
+        self.focusy = y
+        self.zoom = zoom
 
-
-        
+    def resetFocus(self):
+        self.focusx = self._oldfocusx
+        self.focusy = self._oldfocusy
+        self.zoom = self._oldzoom
