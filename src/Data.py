ae3e5f7f574ca0a64de8119480bf17b92a1585a0
nhydock <n_hydock@comcast.net>
2011-09-13 14:42:03 -0400

    update the listpath in data.py to match the one in the engine

--- src/Data.py
+++ src/Data.py
@@ -2,15 +2,33 @@ from sysobj import *
 #for list files
 import glob
 
-def listpath(path, value = ".ini", flag = None):
+#searches path for files with filetype or folder
+def listPath(path, value = "ini", flag = None, exclude = None):
     items = []
-    path = os.path.join("..", path)
-    if flag == "filename":
-        items = [n.replace(value, "") for n in glob.glob(os.path.join(path, "*." + value))]
+    searchpath = os.path.join("..", "data", path)
+    #returns a list of the folders in the path
+    if flag == "folder":
+        items = os.listdir(searchpath)
     else:
-        items = [glob.glob(os.path.join(path, "*." + value))]
-        
+        #allow for multiple endings when searching
+        for val in value.split("|"):
+            #retrieve just the file names
+            if flag == "filename":
+                item = [n.rsplit("/",1)[1].replace("." + value, "") for n in glob.glob(os.path.join(searchpath, "*." + val))]
+            #returns a list of the folders in the path that contain the searched file
+            elif flag == "folderDeepSearch":
+                item = [n for n in os.listdir(searchpath) if os.path.isfile(os.path.join(searchpath, n, val))]
+            #retrieve the entire filename, extension included
+            else:
+                item = [n.rsplit("/",1)[1] for n in glob.glob(os.path.join(searchpath, "*." + val))]
+            for i in item:
+                items.append(i)
+    #removes this file from list
+    if exclude:
+        items.remove(exclude)
+    
     return items
+
 
 class Data:
     def __init__(self):
@@ -20,4 +38,4 @@ class Data:
         self.defaultFont = "default.ttf"
         self.menuFont = "menu.ttf"
 
-        self.towns = listpath(os.path.join("data", "places", "towns"))
+        self.towns = listPath(os.path.join("data", "places", "towns"))
